670a449d61bc3c180205811e7ab6f0e756c4fbe1
RuslanGon <ruslan441006@gmail.com>
2024-07-21 17:52:40 +0300

    wer

--- src/db/initMongoConection.js
+++ src/db/initMongoConection.js
@@ -1,17 +1,22 @@
 import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
-import { ENV_WARS } from '../constants/index.js';
+import { ENV_VARS } from '../constants/index.js';
 
 export const initMongoConection = async () => {
-  const conectionLink = `mongodb+srv://
-  ${env(ENV_WARS.MONGODB_USER)}:
-  ${env(ENV_WARS.MONGODB_PASSWORD)}@
-  ${env(ENV_WARS.MONGODB_URL)}/${env(ENV_WARS.MONGODB_DB)}
-  ?retryWrites=true&w=majority&appName=Cluster0`;
+    const user = env(ENV_VARS.MONGODB_USER);
+    const password = env(ENV_VARS.MONGODB_PASSWORD);
+    const url = env(ENV_VARS.MONGODB_URL);
+    const db = env(ENV_VARS.MONGODB_DB);
 
-  try {
-    await mongoose.connect(conectionLink);
-  } catch (err) {
-    console.log(err);
-  }
-};
+    const conectionLink = `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
+
+    console.log('Connection link:', conectionLink);
+
+    try {
+      await mongoose.connect(conectionLink, { useNewUrlParser: true, useUnifiedTopology: true });
+      console.log('MongoDB connection successful');
+    } catch (err) {
+      console.error('Error connecting to MongoDB:', err);
+  throw err;
+    }
+  };
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { ENV_WARS } from './constants/index.js';
+import { ENV_VARS } from './constants/index.js';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import { notFoundMiddleware } from './middlewares/notFoundMiddleware.js';
 
@@ -29,7 +29,7 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 
-const PORT = env(ENV_WARS.PORT, 3000);
+const PORT = env(ENV_VARS.PORT, 3000);
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
   });
