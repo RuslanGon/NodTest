41223946e3ae4a94d9b16a6a48f0069f038ed92a
RuslanGon <ruslan441006@gmail.com>
2024-07-21 12:20:09 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3001, () => {
-    console.log('server is running on port 3001');
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
   });
 
 
