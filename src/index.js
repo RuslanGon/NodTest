bb4df63738a9b9af523962ac79c1f77467fabeea
RuslanGon <ruslan441006@gmail.com>
2024-07-21 15:15:32 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
+import { startServer } from "./server.js";
 
-const app = express();
-
-app.use(pino({
-  transport: {
-    target: 'pino-pretty'
-  }
-}
-));
-
-app.use(cors());
-
-app.get('/', (req, res, next) => {
-res.send('Hello Ruslan');
-});
-
-app.use((req, res,) => {
-res.status(404).send('Oops! Route is not found');
-
-});
-
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
-  });
-
-
-
+startServer();
