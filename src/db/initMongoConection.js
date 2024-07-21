73fb0106a3fa2fd0b90265cb4d6468cfaf5ea205
RuslanGon <ruslan441006@gmail.com>
2024-07-21 17:40:52 +0300

    wer

--- src/db/initMongoConection.js
+++ src/db/initMongoConection.js
@@ -1,7 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import { env } from '../utils/env.js';
-import { ENV_WARS } from "../constants/index.js";
+import { ENV_WARS } from '../constants/index.js';
 
-export const initMongoConection = () => {
-const conectionLink = `mongodb+srv://${env(ENV_WARS.MONGODB_USER)}:<password>@cluster0.9yeu1fb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+export const initMongoConection = async () => {
+  const conectionLink = `mongodb+srv://
+  ${env(ENV_WARS.MONGODB_USER)}:
+  ${env(ENV_WARS.MONGODB_PASSWORD)}@
+  ${env(ENV_WARS.MONGODB_URL)}/${env(ENV_WARS.MONGODB_DB)}
+  ?retryWrites=true&w=majority&appName=Cluster0`;
+
+  try {
+    await mongoose.connect(conectionLink);
+  } catch (err) {
+    console.log(err);
+  }
 };
