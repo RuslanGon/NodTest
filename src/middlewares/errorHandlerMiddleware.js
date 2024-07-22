8b61971310181953b194ec0c0531016dd695981f
RuslanGon <ruslan441006@gmail.com>
2024-07-22 11:16:16 +0300

    wer

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,5 +1,9 @@
 export const errorHandlerMiddleware = (error, req, res) => {
-  {
-    res.status(500).send(error.message);
-  }
+  res.status(500).json({
+    status: 500,
+    message: 'internal server error',
+    data: {
+message: error.message
+    }
+   });
 };
