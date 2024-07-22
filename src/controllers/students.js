c65769a53f4366ac7b8d5bceb634cdaec07a9e70
RuslanGon <ruslan441006@gmail.com>
2024-07-22 11:09:58 +0300

    wer

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -2,21 +2,15 @@ import { getAllStudents, getStudentById } from "../services/students.js";
 
 
 export const getStudentsController = async (req, res) => {
-  try {
     const students = await getAllStudents();
     res.json({
-      status: 200,
-      message: 'Successful get all students',
-      data: students,
+     status: 200,
+     message: 'Successful get all students',
+     data: students
     });
-  } catch (err) {
-    res.json({
-        status: 500,
-        message: 'Error students',
-        data: err,
-      });
-  }
-};
+   };
+
+
 
 export const getStudentByIdController = async (req, res) => {
     const id = req.params.studentId;
--- src/routers/students.js
+++ src/routers/students.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getStudentByIdController, getStudentsController } from "../controllers/students.js";
+import { ctrlWrapper } from "../middlewares/ctrlWrapper.js";
 
 const studentRouter = Router();
 
 
-studentRouter.get('/students', getStudentsController);
+studentRouter.get('/students', ctrlWrapper(getStudentsController) );
 
-studentRouter.get('/students/:studentId', getStudentByIdController );
+studentRouter.get('/students/:studentId', ctrlWrapper(getStudentByIdController) );
 
 export default  studentRouter;
