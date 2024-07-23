e437653f1f0542726964ec790afc8c95fc3d607e
RuslanGon <ruslan441006@gmail.com>
2024-07-23 11:43:57 +0300

    wer

--- src/validation/createStudentSchema.js
+++ src/validation/createStudentSchema.js
@@ -1,7 +1,13 @@
 import joi from 'joi';
 
 export const createStudentSchema = joi.object({
-name: joi.string().required().min(2).max(17),
+name: joi.string().required().min(2).max(17).message({
+"String required": "Is required",
+"String min": "Min string leght is not achived",
+"String max": "Max string leght more achived",
+
+
+}),
 age: joi.number().integer().required().min(14).max(77),
 gender: joi.string().required().valid('male', 'female', 'other'),
 avgMark: joi.number().required().min(1).max(17),
