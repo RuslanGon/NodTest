5c1f3e920af109b47b895ffb9fb4d41e031f1fc7
RuslanGon <ruslan441006@gmail.com>
2024-07-23 10:25:57 +0300

    wer

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,25 +1,46 @@
-import { Router } from "express";
-import { createStudentController, deleteStudentByIdController, getStudentByIdController, getStudentsController, patchStudentController, putStudentController} from "../controllers/students.js";
-import { ctrlWrapper } from "../middlewares/ctrlWrapper.js";
-import { validationMongoId } from "../validationMongoId.js";
+import { Router } from 'express';
+import {
+  createStudentController,
+  deleteStudentByIdController,
+  getStudentByIdController,
+  getStudentsController,
+  patchStudentController,
+  putStudentController,
+} from '../controllers/students.js';
+import { ctrlWrapper } from '../middlewares/ctrlWrapper.js';
+import { validationMongoId } from '../middlewares/validationMongoId.js';
+
 
 const studentRouter = Router();
 
+studentRouter.get('/students', ctrlWrapper(getStudentsController));
 
-studentRouter.get('/students', ctrlWrapper(getStudentsController) );
+studentRouter.get(
+  '/students/:studentId',
+  validationMongoId,
+  ctrlWrapper(getStudentByIdController),
+);
 
-studentRouter.get('/students/:studentId', validationMongoId, ctrlWrapper(getStudentByIdController) );
+studentRouter.post(
+'/students', ctrlWrapper(createStudentController)
+);
 
-studentRouter.post('/students', ctrlWrapper(createStudentController) );
+studentRouter.patch(
+  '/students/:studentId',
+  validationMongoId,
+  ctrlWrapper(patchStudentController),
+);
 
-studentRouter.patch('/students/:studentId', validationMongoId, ctrlWrapper(patchStudentController) );
+studentRouter.put(
+  '/students/:studentId',
+  validationMongoId,
+  ctrlWrapper(putStudentController),
+);
 
-studentRouter.put('/students/:studentId', validationMongoId, ctrlWrapper(putStudentController) );
+studentRouter.delete(
+  '/students/:studentId',
+  validationMongoId,
+  ctrlWrapper(deleteStudentByIdController),
+);
 
-studentRouter.delete('/students/:studentId', validationMongoId, ctrlWrapper(deleteStudentByIdController) );
-
-
-
-
-
-export default  studentRouter;
+export default studentRouter;
