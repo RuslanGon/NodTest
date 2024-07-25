96a797820d4c8aaa745d136226f30f1c57498a38
RuslanGon <ruslan441006@gmail.com>
2024-07-25 09:55:08 +0300

    wer

---
+++ src/db/models/users.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+export const  usersSchema = new Schema({
+    name: {type: String, required: true},
+    age: {type: Number, required: true},
+    gender: {type: String, required: true, enum: ['male', 'female', 'other']},
+    avgMark: {type: Number, required: true, min: 1, max: 12},
+    onDuty: {type: Boolean, default: false}
+}, {
+timestamps: true,
+versionKey: false
+});
+
+export const User = model('students', usersSchema);
