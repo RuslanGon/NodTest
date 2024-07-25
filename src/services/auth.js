b5cc1e0ff93270b0cefd3c4f54874b242362bca3
RuslanGon <ruslan441006@gmail.com>
2024-07-25 12:10:12 +0300

    wer

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,6 +3,14 @@ import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
 
 export const createUser = async (payload) => {
+
+  const user = await User.findOne({email: payload.email});
+
+  if(!user){
+    throw createHttpError(409, 'User with email is ready present in database');
+  }
+
+
 const hashedPassword = await bcrypt.hash(payload.password, 10);
 
 return await User.create({...payload, password: hashedPassword});
@@ -15,8 +23,10 @@ if(!user){
     throw createHttpError(404, 'User not found');
 }
 
-const isPasswordValid = await user.comparePassword(password);
+const isPasswordValid = await user.comparePassword(password, user.password);
 if (!isPasswordValid) {
   throw createHttpError(401, 'Invalid password');
 }
+
+return user;
 };
