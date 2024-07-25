ec46611a366f36f22c815ba5a4e437c7832e8909
RuslanGon <ruslan441006@gmail.com>
2024-07-25 13:22:24 +0300

    wer

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -13,7 +13,13 @@ data: {user}
 
 export const loginUserController = async (req, res, next) => {
   const session = await loginUser(req.body);
+
   res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 дней
+  });
+
+  res.cookie('sessionToken', session.refreshToken, {
     httpOnly: true,
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 дней
   });
