cc78b1c04fe6928297d616c3e1eea49c6d68a185
RuslanGon <ruslan441006@gmail.com>
2024-07-25 12:41:42 +0300

    wer

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,32 +1,33 @@
 import { User } from "../db/models/user.js";
 import createHttpError from 'http-errors';
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 export const createUser = async (payload) => {
+  const user = await User.findOne({ email: payload.email });
 
-  const user = await User.findOne({email: payload.email});
-
-  if(!user){
-    throw createHttpError(409, 'User with email is ready present in database');
+  if (user) {
+    throw createHttpError(409, 'User with email is already present in database');
   }
 
+  const hashedPassword = await bcrypt.hash(payload.password, 10);
 
-const hashedPassword = await bcrypt.hash(payload.password, 10);
-
-return await User.create({...payload, password: hashedPassword});
+  return await User.create({ ...payload, password: hashedPassword });
 };
 
-
-export const loginUser = async ({email, password}) => {
-const user = await User.findOne({email});
-if(!user){
+export const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+  if (!user) {
     throw createHttpError(404, 'User not found');
-}
+  }
 
-const isPasswordValid = await user.comparePassword(password, user.password);
-if (!isPasswordValid) {
-  throw createHttpError(401, 'Invalid password');
-}
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw createHttpError(401, 'Invalid password');
+  }
 
-return user;
+  const accessToken = crypto.randomBytes(10).toString('base64');
+  const refreshToken = crypto.randomBytes(10).toString('base64');
+
+  return { user, accessToken, refreshToken };
 };
