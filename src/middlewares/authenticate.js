b72a5ebd3200b60f5c82d645f532f123b9f1db9d
RuslanGon <ruslan441006@gmail.com>
2024-07-26 19:12:45 +0300

    wer

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,10 +1,23 @@
 import createHttpError from 'http-errors';
+import { Session } from '../db/models/session.js';
 
 
-export const authenticate = (req, res, next) => {
+export const authenticate = async (req, res, next) => {
 const header = req.get('Autorization');
 if(!header){
-throw createHttpError(401, 'Auth header is not provided')
+throw createHttpError(401, 'Auth header is not provided');
 }
 const [bearer, token] = header.split(' ');
+
+if(bearer !== 'Bearer' || !token ){
+    throw createHttpError(401, 'Auth header should be of bearer type');
+}
+
+const session = await Session.findOne({
+accessToken: token
+});
+
+if(!session){
+    throw createHttpError(401, 'Auth header should be of bearer type');
+}
 };
