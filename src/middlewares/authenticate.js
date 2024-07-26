79a2a095e645309046ff7559710802c7aaf488bb
RuslanGon <ruslan441006@gmail.com>
2024-07-26 19:24:52 +0300

    wer

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,21 +3,33 @@ import { Session } from '../db/models/session.js';
 
 
 export const authenticate = async (req, res, next) => {
-const header = req.get('Autorization');
-if(!header){
-next(createHttpError(401, 'Auth header is not provided')) ;
-}
-const [bearer, token] = header.split(' ');
+  const header = req.get('Authorization');
+  if (!header) {
+   return next(createHttpError(401, 'Auth header is not provided'));
+  }
+  const [bearer, token] = header.split(' ');
 
-if(bearer !== 'Bearer' || !token ){
-next(createHttpError(401, 'Auth header should be of bearer type')) ;
-}
+  if (bearer !== 'Bearer' || !token) {
+    return next(createHttpError(401, 'Auth header should be of bearer type'));
+  }
 
-const session = await Session.findOne({
-accessToken: token
-});
+  const session = await Session.findOne({
+    accessToken: token,
+  });
 
-if(!session){
-    throw createHttpError(401, 'Session not found');
-}
+  if (!session) {
+    return next(createHttpError(401, 'Session not found'));
+  }
+
+  if(Date.now() > session.accessTokenValidUntil){
+    next(createHttpError(401, 'Session token is expired'));
+  }
+
+  const user = await Session.findById(session.userId);
+
+  if(!user){
+    return next(createHttpError(401, 'User associated with this session is not found'));
+  }
+  req.user = user;
+next();
 };
