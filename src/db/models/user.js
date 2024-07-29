281f412837e45e9b5849539e0e7f76c03e374322
RuslanGon <ruslan441006@gmail.com>
2024-07-29 16:53:29 +0300

    wer

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -4,6 +4,8 @@ export const userSchema = new Schema({
     name: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    role: { type: String, required: true, default: ['parent'], enum: ['parent', 'teacher'] },
+
 }, {
     timestamps: true,
     versionKey: false
