89be5af01ee55b213939a418868a151ce8db9538
RuslanGon <ruslan441006@gmail.com>
2024-07-30 16:39:57 +0300

    wer

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 const transport = nodemailer.createTransport({
   host: 'smtp.example.com',
   port: 587,
-  tls: {
-    rejectUnauthorized: true,
-    minVersion: 'TLSv1.2',
-  },
+  auth: {
+user: 'user',
+pass: 'pass'
+  }
 });
