2c7a66c62dd1e8ccbaca89b080803f0f2aa841f2
RuslanGon <ruslan441006@gmail.com>
2024-07-30 17:04:43 +0300

    wer

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,8 +1,9 @@
 import nodemailer from 'nodemailer';
 import { ENV_VARS } from '../constants/index.js';
+import { env } from '../utils/env.js';
 
 const transport = nodemailer.createTransport({
-  host: ENV_VARS,
+  host: env(ENV_VARS),
   port: 587,
   auth: {
 user: 'user',
