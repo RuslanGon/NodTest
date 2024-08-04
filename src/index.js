52845daf6066e0a90b5bf4f72e4cf2c42f4f1bb7
RuslanGon <ruslan441006@gmail.com>
2024-08-04 11:56:49 +0300

    wer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import { TEMPLATE_UPLOAD_DIR } from "./constants/index.js";
 import { initMongoConection } from "./db/initMongoConection.js";
 // import { Student } from "./db/models/student.js";
 import { startServer } from "./server.js";
+import { createFolder } from "./utils/createFolder.js";
 
 ( async() => {
     await initMongoConection();
+    await createFolder(TEMPLATE_UPLOAD_DIR);
     // const students = await Student.find({});
     // console.log(students);
     startServer();
