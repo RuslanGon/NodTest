a363d62c779f1af8305186e7f8e8e2e0c49b75e8
RuslanGon <ruslan441006@gmail.com>
2024-08-04 12:45:17 +0300

    wer

--- src/utils/saveFileToLS.js
+++ src/utils/saveFileToLS.js
@@ -3,8 +3,14 @@ import path from 'node:path';
 import { UPLOAD_DIR } from '../constants/index.js';
 
 export const saveFileToLS = async (file) => {
-const content = await fs.readFile(file.path);
-const newPath = path.join(UPLOAD_DIR, file.filename);
-await fs.writeFile(newPath, content);
-return 'url';
+  // Чтение содержимого файла с оригинального пути
+  const content = await fs.readFile(file.path);
+
+  // Создание нового пути для файла в директории UPLOAD_DIR
+  const newPath = path.join(UPLOAD_DIR, file.filename);
+
+  // Запись содержимого в новый файл
+  await fs.writeFile(newPath, content);
+
+  return newPath;
 };
