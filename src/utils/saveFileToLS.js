9a49995ebac58560f3e0c039ff6b2b68c990b723
RuslanGon <ruslan441006@gmail.com>
2024-08-04 12:42:49 +0300

    wer

--- src/utils/saveFileToLS.js
+++ src/utils/saveFileToLS.js
@@ -1,3 +1,10 @@
-export const saveFileToLS = (file) => {
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { UPLOAD_DIR } from '../constants/index.js';
 
+export const saveFileToLS = async (file) => {
+const content = await fs.readFile(file.path);
+const newPath = path.join(UPLOAD_DIR, file.filename);
+await fs.writeFile(newPath, content);
+return 'url';
 };
