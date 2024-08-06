41eb00afea7786ffbf2a17c1b183ee68e0ca6ed3
RuslanGon <ruslan441006@gmail.com>
2024-08-06 17:29:51 +0300

    wer

---
+++ src/utils/saveToCloudinary.js
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'di6dcnxjk',
+    api_key: '391597636912541',
+    api_secret: '<your_api_secret>' // Click 'View Credentials' below to copy your API secret
+});
+
+export const saveToCloudinary = (file) => {
+return 'url';
+};
