8be9b1ed49474b95e0c5e3627f101401414e51c7
RuslanGon <ruslan441006@gmail.com>
2024-08-06 18:42:59 +0300

    wer

--- src/utils/saveFile.js
+++ src/utils/saveFile.js
@@ -1,1 +1,13 @@
-export const saveFile = () => {};
+import { env } from '../utils/env.js';
+import { ENV_VARS } from '../constants/index.js';
+import { saveToCloudinary } from './saveToCloudinary.js';
+
+export const saveFile = async (file) => {
+  let url;
+
+  if (env(ENV_VARS.IS_CLOUDINARY_ENABLED)) {
+    url = await saveToCloudinary;
+  }
+
+  return url;
+};
