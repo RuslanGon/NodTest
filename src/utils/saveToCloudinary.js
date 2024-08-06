b1a43970fd33262a50ba1e839040d94442ac9433
RuslanGon <ruslan441006@gmail.com>
2024-08-06 18:15:39 +0300

    wer

--- src/utils/saveToCloudinary.js
+++ src/utils/saveToCloudinary.js
@@ -1,14 +1,18 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { env } from '../utils/env.js';
 import { ENV_VARS } from '../constants/index.js';
+import fs from 'node:fs/promises';
 
 cloudinary.config({
     cloud_name: env(ENV_VARS.CLOUD_NAME),
     api_key: env(ENV_VARS.API_KEY),
-    api_secret: env(ENV_VARS.API_SECRET) // Click 'View Credentials' below to copy your API secret
+    api_secret: env(ENV_VARS.API_SECRET)
 });
 
 
-export const saveToCloudinary = (file) => {
-return 'url';
+export const saveToCloudinary = async (file) => {
+  const content = await fs.readFile(file.path);
+
+  const res = await cloudinary.upload(content);
+  return res.secure_url;
 };
