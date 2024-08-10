5de24d9458c89c7d87b9324f986f4d69fc2a2b8f
RuslanGon <ruslan441006@gmail.com>
2024-08-10 12:11:49 +0300

    wer

--- src/utils/googleOauht.js
+++ src/utils/googleOauht.js
@@ -1,6 +1,12 @@
 import { OAuth2Client } from 'google-auth-library';
+import fs from 'node:fs';
+import path from 'node:path';
 import { env } from './env.js';
 import { ENV_VARS } from '../constants/index.js';
+
+const googleConfig = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), 'google.json')).toString(),
+);
 
 const client = new OAuth2Client({
 clientId: env(ENV_VARS.GOOGLE_CLIENT_ID),
